0a4d1dc15d56d0c3d4f9cee860de5a11d68ce0f9
Govind <singlagovind798@gmail.com>
2021-01-07 15:00:06 +0530

    Initial Commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author GOVIND SINGLA
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        calculator_main cm=new calculator_main();
+        cm.setVisible(true);
+    
+    }
+    
+}
